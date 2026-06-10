b23359b9f334f7504e9f648b6a6b4d943f404140
bhav-yeah <107114820+bhav-yeah@users.noreply.github.com>
2026-06-10 08:10:38 +0200

    Guard FlutterImagePool async completions after dispose (#3)
    * Add disposal checks to FlutterImagePool to prevent async callbacks after disposal
    
    * Add unit tests for handling late asynchronous completions after pool disposal

--- test/flutter_image_pool_test.dart
+++ test/flutter_image_pool_test.dart
@@ -1,5 +1,6 @@
 // Path: test/flutter_image_pool_test.dart
 
+import 'dart:async';
 import 'package:canvas_core/canvas_core_runtime.dart';
 import 'package:canvas_renderer_flutter/src/images/flutter_image_intrinsics.dart';
 import 'package:canvas_renderer_flutter/src/images/flutter_image_pool.dart';
@@ -130,5 +131,47 @@ void main() {
         pool.dispose();
       },
     );
+
+    test('ignores late unresolved preload completion after dispose', () async {
+      final resolver = Completer<Map<String, String>>();
+
+      final pool = FlutterImagePool(assetUrlsResolver: (_) => resolver.future);
+      final intrinsics = FlutterImageIntrinsics(pool.images);
+
+      final future = pool.preloadScene(
+        _sceneWithImages(['media:late']),
+        intrinsics: intrinsics,
+      );
+
+      pool.dispose();
+
+      resolver.complete(const <String, String>{});
+
+      await expectLater(future, completes);
+      expect(pool.images, isEmpty);
+
+      intrinsics.dispose();
+    });
+
+    test('ignores late intrinsic metadata completion after dispose', () async {
+      final resolver = Completer<Map<String, Size2D>>();
+
+      final pool = FlutterImagePool(assetMetasResolver: (_) => resolver.future);
+      final intrinsics = FlutterImageIntrinsics(pool.images);
+
+      final future = pool.resolveSceneIntrinsics(
+        _sceneWithImages(['media:late']),
+        intrinsics: intrinsics,
+      );
+
+      pool.dispose();
+
+      resolver.complete(const <String, Size2D>{'media:late': Size2D(640, 480)});
+
+      await expectLater(future, completes);
+      expect(intrinsics.intrinsicSize('image-0'), isNull);
+
+      intrinsics.dispose();
+    });
   });
 }
